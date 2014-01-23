300b2733c4a41bbe012400616511eb68b894c8ea
LightGuard <lightguard.jp@gmail.com>
2014-01-23 14:11:31 -0700

    Some renaming, droping of tests and other identity bits
    Also adding some documentation

--- lib/aweplug/helpers/git_metadata.rb
+++ lib/aweplug/helpers/git_metadata.rb
@@ -4,11 +4,23 @@ require 'json'
 module Aweplug
   module Helper
     module Git
+      module Repository
+        # Public: Returns the URL for the git repository.
+        #
+        # repo_root - The directory (relative to the site base) containing the 
+        #             git repository.
+        # remote_name - Name of the remote to retrieve, defaults to 'origin'
+        #
+        # Returns a string containing the URL of the git remote repository
+        def repository_url(repo_root, remote_name='origin') 
+          Open3.capture2(%Q[git --git-dir=#{repo_root}/.git config --get remote.#{remote_name}.url]).first.chomp[0..-5]
+        end
+      end
       module Commit
         module Metadata
           # Public: Retrieves commit information from the git repo containing the file.
           #
-          # repo_root - The directory (relative to the site base) containing  the git repo
+          # repo_root - The directory (relative to the site base) containing the git repo
           # file_path - Path to the file being processed, relative to the site base
           #
           # Returns an array of commit info as json values
@@ -21,3 +33,4 @@ module Aweplug
     end
   end
 end
+
