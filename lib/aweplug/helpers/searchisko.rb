d783e8f0cadc6b0c47f559c3583cf724a922a73a
LightGuard <lightguard.jp@gmail.com>
2014-01-23 15:40:31 -0700

    Documentation!
    All of the classes should now contain documentation.

--- lib/aweplug/helpers/searchisko.rb
+++ lib/aweplug/helpers/searchisko.rb
@@ -14,6 +14,8 @@ module Aweplug::Helpers
     #        :logging - Boolean to log responses
     #        :raise_error - Boolean flag if 404 and 500 should raise exceptions
     #        :adapter - faraday adapter to use, defaults to :net_http
+    #
+    # Returns a new instance of Searchisko.
     def initialize opts={} 
       @faraday = Faraday.new(:url => opts[:base_url]) do |builder|
         if opts[:authenticate]
@@ -30,18 +32,65 @@ module Aweplug::Helpers
       end
     end
 
+    # Public: Performs a GET search against the Searchisko instance using 
+    # provided parameters.
+    #
+    # params - Hash of parameters to use as query string. See
+    #          http://docs.jbossorg.apiary.io/#searchapi for more information
+    #          about parameters and how they affect the search.
+    #
+    # Example
+    #
+    #   searchisko.search {:query => 'Search query'}
+    #   # => {...}
+    #
+    # Returns the String result of the search.
     def search params = {}
       get '/search', params
     end
 
+    # Public: Makes an HTTP GET to host/v1/rest/#{path} and returns the 
+    # result from the Faraday request.
+    #
+    # path   - String containing the rest of the path.
+    # params - Hash containing query string parameters.
+    #
+    # Example
+    #   
+    #   searchisko.get 'feed', {:query => 'Search Query'}
+    #   # => Faraday Response Object
+    #
+    # Returns the Faraday Response for the request.
     def get path, params = {}
       @faraday.get "/v1/rest/" + path, params
     end
 
+    # Public: Posts content to Searchisko.
+    #
+    # content_type - String of the Searchisko sys_content_type for the content 
+    #                being posted.
+    # content_id   - String of the Searchisko sys_content_id for the content.
+    # params       - Hash containing the content to push.
+    #
+    # Examples
+    #
+    #   searchisko.push_content 'jbossdeveloper_bom', id, content_hash
+    #   # => Faraday Response
+    #
+    # Returns a Faraday Response from the POST.
     def push_content content_type, content_id, params = {}
       post "/content/#{content_type}/#{content_id}", params
     end
 
+    # Public: Perform an HTTP POST to Searchisko.
+    #
+    # path   - String containing the rest of the path.
+    # params - Hash containing the POST body.
+    #
+    # Examples
+    #
+    #   searchisko.post "rating/#{searchisko_document_id}", {rating: 3}
+    #   # => Faraday Response
     def post path, params = {}
       @faraday.post do |req|
         req.url "/v1/rest/" + path
