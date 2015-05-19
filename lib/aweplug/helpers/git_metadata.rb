0b2879c4742ac14b26e0785f06d5707d42c757f6
LightGuard <lightguard.jp@gmail.com>
2015-05-19 14:39:47 -0600

    Add ability for submodules in .git or module dir
    We've found depending on the version the submodule .git directory can be
    in either <submodule>/.git or <root>/.git/module. This should work
    regardless of which way the submodules are created.
    
    Also removed an .orig file that shouldn't have been there.
    
    Trying new idea to find git directory
    
    Removing puts

--- lib/aweplug/helpers/git_metadata.rb
+++ lib/aweplug/helpers/git_metadata.rb
@@ -5,7 +5,26 @@ require 'yaml'
 module Aweplug
   module Helper
     module Git
+      # Public: Finds the correct repository directory (.git / .git/modules).
+      #
+      # repo_root - The directory containing the git repository
+      #
+      # Returns a string containing the correct location of the git repo.
+      def repo_dir(repo_root)
+        return File.join(repo_root, '.git') if File.directory? File.join(repo_root, '.git')
+        if File.file? File.join(repo_root, '.git')
+          gitdir = File.join(repo_root, YAML.load_file(File.join(repo_root, '.git'))['gitdir'])
+          if File.exists? gitdir
+            return gitdir
+          else
+            raise "Could not find git directory for #{repo_root}"
+          end
+        end
+      end
+
       module Repository
+        include Aweplug::Helper::Git
+
         # Public: Returns the URL for the git repository.
         #
         # repo_root - The directory (relative to the site base) containing the 
@@ -14,11 +33,13 @@ module Aweplug
         #
         # Returns a string containing the URL of the git remote repository
         def repository_url(repo_root, remote_name='origin') 
-          Open3.capture2(%Q[git --git-dir=#{repo_root}/.git config --get remote.#{remote_name}.url]).first.chomp[0..-5]
+          Open3.capture2(%Q[git --git-dir=#{repo_dir(repo_root)} config --get remote.#{remote_name}.url]).first.chomp[0..-5]
         end
       end
       module Commit
         module Metadata
+          include Aweplug::Helper::Git
+
           # Public: Retrieves commit information from the git repo containing the file.
           #
           # repo_root - The directory (relative to the site base) containing the git repo
@@ -31,7 +52,7 @@ module Aweplug
             default_opts = {date: 'iso', format: %Q|{"author":"%an","author_email":"%ae","date":"%ai","hash":"%h","subject":"%f"}| }
             opts.merge! default_opts
 
-            cmd = %Q[git --git-dir=#{repo_root}/.git log]
+            cmd = %Q[git --git-dir=#{repo_dir(repo_root)} log]
             opts.each {|key,value| cmd << " --#{key}='#{value}'"}
             cmd << " -- #{file_path.to_s.sub(/#{repo_root}\//, '')}"
 
@@ -47,7 +68,7 @@ module Aweplug
           #
           # Returns the most recent tag
           def current_tag (repo_root, file_path)
-            o, _ = Open3.capture2(%Q[git --git-dir=#{repo_root}/.git describe --tags --always])
+            o, _ = Open3.capture2(%Q[git --git-dir=#{repo_dir(repo_root)} describe --tags --always])
             o.strip
           end
 
@@ -59,7 +80,7 @@ module Aweplug
           #
           # Returns the current branch
           def current_branch (repo_root, file_path)
-            o, _ = Open3.capture2(%Q[git --git-dir=#{repo_root}/.git rev-parse --abbrev-ref HEAD])
+            o, _ = Open3.capture2(%Q[git --git-dir=#{repo_dir(repo_root)} rev-parse --abbrev-ref HEAD])
             o.strip
           end
         end
